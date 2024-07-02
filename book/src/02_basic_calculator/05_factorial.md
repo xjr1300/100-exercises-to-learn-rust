94d53543a5b7ddd8266448ede8b071c64a67085b
kuroyasu <xjr1300.04@gmail.com>
2024-07-02 14:16:47 +0900

    02_basic_calculator / 06_factorial

--- book/src/02_basic_calculator/05_factorial.md
+++ book/src/02_basic_calculator/05_factorial.md
@@ -1,4 +1,4 @@
-# Factorial
+# Factorial（階乗）
 
 So far you've learned:
 
@@ -9,3 +9,13 @@ So far you've learned:
 - How to execute conditional logic via comparisons and `if`/`else` expressions
 
 It looks like you're ready to tackle factorials!
+
+> これまで、次を学びました。
+>
+> - 関数を定義する方法
+> - 関数を呼び出す方法
+> - Rustで利用可能な整数型
+> - 整数のために利用できる算術演算子
+> - 比較と`if`/`else`式を介して条件的なロジックを実行する方法
+>
+> 階乗に取り組む準備ができたようです！
--- exercises/02_basic_calculator/05_factorial/src/lib.rs
+++ exercises/02_basic_calculator/05_factorial/src/lib.rs
@@ -1,14 +1,30 @@
 // Define a function named `factorial` that, given a non-negative integer `n`,
 // returns `n!`, the factorial of `n`.
+// 非負の整数`n`を受け取り、`n`の階乗である`n!`を返す`factorial`と名付けた関数を定義してください。
 //
 // The factorial of `n` is defined as the product of all positive integers up to `n`.
 // For example, `5!` (read "five factorial") is `5 * 4 * 3 * 2 * 1`, which is `120`.
 // `0!` is defined to be `1`.
+// `n`の階乗は、`n`までの正の整数の積として定義されます。
+// 例えば、`5!`（「5の階乗」と読みます）は、`5 * 4 * 3 * 2 * 1`で、それは`120`です。
+// `0!`は`1`と定義されています。
 //
 // We expect `factorial(0)` to return `1`, `factorial(1)` to return `1`,
 // `factorial(2)` to return `2`, and so on.
+// `factorial(0)`が`1`を返し、`factorial(1)`が`1`を返し、`factorial(2)`が`2`を返す、といったことを期待します。
 //
 // Use only what you learned! No loops yet, so you'll have to use recursion!
+// 学んだことだけを歯悠桜してください！ループはまだなので、再帰を使用する必要があります。
+
+fn factorial(n: u32) -> u32 {
+    if n == 0 {
+        1
+    } else if n == 1 {
+        1
+    } else {
+        n * factorial(n - 1)
+    }
+}
 
 #[cfg(test)]
 mod tests {
