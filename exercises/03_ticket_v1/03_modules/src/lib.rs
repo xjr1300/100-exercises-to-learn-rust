1324d52c872815250713180ecb509742dfbde9fa
Ishiguro Yasunori <xjr1300.04@gmail.com>
2024-07-03 15:36:00 +0900

    03_ticket_v1 / 03_modules

--- exercises/03_ticket_v1/03_modules/src/lib.rs
+++ exercises/03_ticket_v1/03_modules/src/lib.rs
@@ -1,6 +1,8 @@
 mod helpers {
     // TODO: Make this code compile, either by adding a `use` statement or by using
     //  the appropriate path to refer to the `Ticket` struct.
+    // `use`文を追加するか、`Ticket`構造体を参照する適切なパスを使用して、コンパイルできるようにしてください。
+    use super::Ticket;
 
     fn create_todo_ticket(title: String, description: String) -> Ticket {
         Ticket::new(title, description, "To-Do".into())
