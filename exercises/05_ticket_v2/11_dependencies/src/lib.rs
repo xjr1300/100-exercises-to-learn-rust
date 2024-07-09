45b1c94ca3250d47e919c8056bf977f0390ab8af
kuroyasu <xjr1300.04@gmail.com>
2024-07-09 14:28:26 +0900

    05_ticket_v2 / 11_dependencies

--- exercises/05_ticket_v2/11_dependencies/src/lib.rs
+++ exercises/05_ticket_v2/11_dependencies/src/lib.rs
@@ -1,6 +1,9 @@
 // TODO: Add `anyhow` as a dependency of this project.
 //  Don't touch this import!
+// このプロジェクトの依存関係として、`anyhow`を追加してください。
+// 次のインポートに触ってはいけません。
 
 // When you import a type (`Error`) from a dependency, the import path must start
 // with the crate name (`anyhow`, in this case).
+// 依存関係から`Error`型をインポートしたとき、そのインポートパスはクレート名で開始しなければ成りません（この場合は`anyhow`です）。
 use anyhow::Error;
