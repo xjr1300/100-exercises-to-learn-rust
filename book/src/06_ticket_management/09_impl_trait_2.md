1f6e80eb45f6688033861d813dd279be775db62d
kuroyasu <xjr1300.04@gmail.com>
2024-07-11 10:16:00 +0900

    06_ticket_management / 09_impl_trait_2

--- book/src/06_ticket_management/09_impl_trait_2.md
+++ book/src/06_ticket_management/09_impl_trait_2.md
@@ -1,7 +1,10 @@
-# `impl Trait` in argument position
+# `impl Trait` in argument position（引数の位置にあるimpl Trait）
 
 In the previous section, we saw how `impl Trait` can be used to return a type without specifying its name.\
 The same syntax can also be used in **argument position**:
+
+> 前のセクションにおいて、その名前を指定しないで型を返すために`impl Trait`を使用する方法を確認しました。
+> 同じ構文は、**引数の位置**でも使用できます。
 
 ```rust
 fn print_iter(iter: impl Iterator<Item = i32>) {
@@ -14,8 +17,11 @@ fn print_iter(iter: impl Iterator<Item = i32>) {
 `print_iter` takes an iterator of `i32`s and prints each element.\
 When used in **argument position**, `impl Trait` is equivalent to a generic parameter with a trait bound:
 
+> `print_iter`は、`i32`のイテレーターを受け取り、それぞれの要素を出力します。
+> **引数の位置**で使用された場合、`impl Trait`はトレイト境界を持つジェネリックなパラメーターと同じです。
+
 ```rust
-fn print_iter<T>(iter: T) 
+fn print_iter<T>(iter: T)
 where
     T: Iterator<Item = i32>
 {
@@ -25,8 +31,12 @@ where
 }
 ```
 
-## Downsides
+## Downsides（欠点）
 
 As a rule of thumb, prefer generics over `impl Trait` in argument position.\
 Generics allow the caller to explicitly specify the type of the argument, using the turbofish syntax (`::<>`),
 which can be useful for disambiguation. That's not the case with `impl Trait`.
+
+> 経験則として、引数の位置に`impl Trait`よりもジェネリックを好むようにしてください。
+> ジェネリックは、ターボフィッシュ構文（`::<>`）を使用して、呼び出し側に引数の型を明示的に指定できるようにして、曖昧さを解消するために役立ちます。
+> それは`impl Trait`を使用したケースには当てはまりません。
--- exercises/06_ticket_management/09_impl_trait_2/src/lib.rs
+++ exercises/06_ticket_management/09_impl_trait_2/src/lib.rs
@@ -1,5 +1,6 @@
 // TODO: Rework the signature of `TicketStore::add_ticket` to use a generic type parameter rather
 //  than `impl Trait` syntax.
+// `impl Trait`構文ではなくジェネリックパラメーターを使用して`TicketStore::add_ticket`のシグネチャーを再加工してください。
 
 use ticket_fields::{TicketDescription, TicketTitle};
 
@@ -33,9 +34,21 @@ impl TicketStore {
     // that can be infallibly converted into a `Ticket`.
     // This can make it nicer to use the method, as it removes the syntax noise of `.into()`
     // from the calling site. It can worsen the quality of the compiler error messages, though.
-    pub fn add_ticket(&mut self, ticket: impl Into<Ticket>) {
+    // `ticket`に対して型パラメーターとして`Into<Ticket>`を使用することは、メソッドが失敗することなく`Ticket`に変換できる
+    // 任意の型を受け付けるようにします。
+    // これは、呼び出し側から`.into()`の構文のノイズを除去するため、メソッドを使用しやすくします。
+    // ただし、コンパイラーのエラーメッセージの品質が悪くなる可能性があります。
+    pub fn add_ticket<T>(&mut self, ticket: T)
+    where
+        T: Into<Ticket>,
+    {
         self.tickets.push(ticket.into());
     }
+    /*
+    pub fn add_ticket<T: Into<Ticket>>(&mut self, ticket: T) {
+        self.tickets.push(ticket.into());
+    }
+    */
 }
 
 #[cfg(test)]
@@ -62,6 +75,7 @@ mod tests {
     fn generic_add() {
         let mut store = TicketStore::new();
         // This won't compile if `add_ticket` uses `impl Trait` syntax in argument position.
+        // `add_ticket`が引数の位置に`impl Trait`構文を使用している場合、これはコンパイルされません。
         store.add_ticket::<TicketDraft>(TicketDraft {
             title: ticket_title(),
             description: ticket_description(),
