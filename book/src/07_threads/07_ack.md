2dfa3d4972c4f7c1b7ec0cbc2f46f665323d1704
kuroyasu <xjr1300.04@gmail.com>
2024-07-12 17:05:53 +0900

    07_threads / 07_ack

--- book/src/07_threads/07_ack.md
+++ book/src/07_threads/07_ack.md
@@ -1,16 +1,28 @@
-# Two-way communication
+# Two-way communication（双方向通信）
 
 In our current client-server implementation, communication flows in one direction: from the client to the server.\
 The client has no way of knowing if the server received the message, executed it successfully, or failed.
 That's not ideal.
 
+> 現在のクライアントサーバーの実装において、通信の流れはクライアントからサーバーへ向かう一方向です。
+> サーバーがメッセージを受け取った場合、正常に実行されたかそれとも失敗したか、クライアントは知る方法がありません。
+> これは理想的てはありません。
+
 To solve this issue, we can introduce a two-way communication system.
 
-## Response channel
+> この問題を解決するために、双方向通信システムを導入できます。
+
+## Response channel（応答チャネル）
 
 We need a way for the server to send a response back to the client.\
 There are various ways to do this, but the simplest option is to include a `Sender` channel in
 the message that the client sends to the server. After processing the message, the server can use
 this channel to send a response back to the client.
 
+> サーバーがクライアントに応答を送信する方法が必要です。
+> これを行う様々な方法がありますが、最も単純な選択肢は、クライアントがサーバーに送信するメッセージ内に`Sender`チャネルを含めることです。
+> メッセージを処理した後、サーバーはクライアントにレスポンスを送信するために、このチャネルを使用できます。
+
 This is a fairly common pattern in Rust applications built on top of message-passing primitives.
+
+> これは、メッセージパッシング構成要素の上に構築されたRustアプリケーションの中で、かなり一般的なパターンです。
--- exercises/07_threads/07_ack/src/lib.rs
+++ exercises/07_threads/07_ack/src/lib.rs
@@ -1,39 +1,64 @@
+use data::TicketDraft;
 use std::sync::mpsc::{Receiver, Sender};
+use store::TicketId;
+
 use crate::store::TicketStore;
 
 pub mod data;
 pub mod store;
 
+use data::Ticket;
+
 // Refer to the tests to understand the expected schema.
+// 期待されるスキーマを理解するためにテストを参照してください。
 pub enum Command {
-    Insert { todo!() },
-    Get { todo!() }
+    Insert {
+        draft: TicketDraft,
+        response_sender: Sender<TicketId>,
+    },
+    Get {
+        id: TicketId,
+        response_sender: Sender<Option<Ticket>>,
+    },
 }
 
 pub fn launch() -> Sender<Command> {
     let (sender, receiver) = std::sync::mpsc::channel();
     std::thread::spawn(move || server(receiver));
+
     sender
 }
 
 // TODO: handle incoming commands as expected.
+// 期待された入ってくるコマンドを処理してください。
 pub fn server(receiver: Receiver<Command>) {
     let mut store = TicketStore::new();
     loop {
         match receiver.recv() {
-            Ok(Command::Insert {}) => {
-                todo!()
+            Ok(Command::Insert {
+                draft,
+                response_sender,
+            }) => {
+                let ticket_id = store.add_ticket(draft);
+                response_sender
+                    .send(ticket_id)
+                    .expect("Did you actually spawn a thread? The channel is closed!");
             }
             Ok(Command::Get {
-                todo!()
+                id,
+                response_sender,
             }) => {
-                todo!()
+                let ticket = store.get(id).cloned();
+                response_sender
+                    .send(ticket)
+                    .expect("Did you actually spawn a thread? The channel is closed!");
             }
             Err(_) => {
                 // There are no more senders, so we can safely break
                 // and shut down the server.
-                break
-            },
+                // 送信者がいないため、安全に閉じて、サーバーをシャットダウンします。
+                break;
+            }
         }
     }
 }
