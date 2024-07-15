1439e5771c096cbd8219ebaad57a776917655d9d
kuroyasu <xjr1300.04@gmail.com>
2024-07-15 15:49:29 +0900

    08_futures / 06_async_aware_primitives

--- book/src/08_futures/06_async_aware_primitives.md
+++ book/src/08_futures/06_async_aware_primitives.md
@@ -1,18 +1,26 @@
-# Async-aware primitives
+# Async-aware primitives（非同期を知っている構成要素）
 
 If you browse `tokio`'s documentation, you'll notice that it provides a lot of types
 that "mirror" the ones in the standard library, but with an asynchronous twist:
 locks, channels, timers, and more.
 
+> `tokio`ドキュメントを参照した場合、標準ライブラリのモノを「反映した」多くの方が提供されていることに気付くでしょうが、非同期になっています。ロック、チャネル、タイマー、その他。
+
 When working in an asynchronous context, you should prefer these asynchronous alternatives
 to their synchronous counterparts.
+
+> 非同期コンテキストで作業するとき、それらの同期的な同等物ではなく、これら非同期の代替物を優先するべきです。
 
 To understand why, let's take a look at `Mutex`, the mutually exclusive lock we explored
 in the previous chapter.
 
-## Case study: `Mutex`
+> 理由を理解するために、前の章で探求した可変排他ロックである`Mutex`を確認しましょう。
+
+## Case study: `Mutex`（事例研究: Mutex）
 
 Let's look at a simple example:
+
+> 単純な例を確認してください。
 
 ```rust
 use std::sync::{Arc, Mutex};
@@ -22,31 +30,41 @@ async fn run(m: Arc<Mutex<Vec<u64>>>) {
     http_call(&guard).await;
     println!("Sent {:?} to the server", &guard);
     // `guard` is dropped here
+    // `guard`はここでドロップされます。
 }
 
 /// Use `v` as the body of an HTTP call.
+/// HTTP呼び出しの本体として`v`を使用します。
 async fn http_call(v: &[u64]) {
   // [...]
 }
 ```
 
-### `std::sync::MutexGuard` and yield points
+### `std::sync::MutexGuard` and yield points（std::sync::MutexGuardとyieldポイント）
 
 This code will compile, but it's dangerous.
+
+> このコードはコンパイルされますが、危険です。
 
 We try to acquire a lock over a `Mutex` from `std` in an asynchronous context.
 We then hold on to the resulting `MutexGuard` across a yield point (the `.await` on
 `http_call`).
 
+> `std`の非同期コンテキストからきた`Mutex`を介してロックを取得することを試みます。
+> そして、`http_call`を`.await`している`yield`ポイントをまたいで結果である`MutexGuard`を保持します。
+
 Let's imagine that there are two tasks executing `run`, concurrently, on a single-threaded
 runtime. We observe the following sequence of scheduling events:
 
+> 単一スレッドなランタイムで、同時並行で`run`を実行する2つのタスクがある場合を想像してください。
+> イベントのスケジューリングの次のＣ館すを観察します。
+
 ```text
      Task A          Task B
-        | 
+        |
   Acquire lock
 Yields to runtime
-        | 
+        |
         +--------------+
                        |
              Tries to acquire lock
@@ -56,9 +74,17 @@ We have a deadlock. Task B we'll never manage to acquire the lock, because the l
 is currently held by task A, which has yielded to the runtime before releasing the
 lock and won't be scheduled again because the runtime cannot preempt task B.
 
+> デッドロックが発生します。
+> タスクBは決してロックを取得できません。
+> 現在、ロックはタスクAによって保持されており、タスクAはロックを開放する前にランタイムに移譲しますが、単ライムはタスクBを実行できないため、再度スケジュールされることはありません。
+
+> タスクBがロックを取得しようとしても、タスクAがロックを取得しているため、タスクBは`let guard = m.lock().unwrap();`でブロックされる。
+
 ### `tokio::sync::Mutex`
 
 You can solve the issue by switching to `tokio::sync::Mutex`:
+
+> `tokio::sync::Mutex`に切り替えることで、その問題を解決できます。
 
 ```rust
 use std::sync::Arc;
@@ -76,13 +102,16 @@ Acquiring the lock is now an asynchronous operation, which yields back to the ru
 if it can't make progress.\
 Going back to the previous scenario, the following would happen:
 
+> 現在、ロックの取得は非同期操作で、それが進めることができない場合、（制御を）ランタイムに移譲します。
+> 前のシナリオに戻ると、次が発生します。
+
 ```text
        Task A          Task B
-          | 
+          |
   Acquires the lock
   Starts `http_call`
   Yields to runtime
-          | 
+          |
           +--------------+
                          |
              Tries to acquire the lock
@@ -91,7 +120,7 @@ Going back to the previous scenario, the following would happen:
                          |
           +--------------+
           |
-`http_call` completes      
+`http_call` completes
   Releases the lock
    Yield to runtime
           |
@@ -103,7 +132,9 @@ Going back to the previous scenario, the following would happen:
 
 All good!
 
-### Multithreaded won't save you
+> すべてがうまくいきます！
+
+### Multithreaded won't save you（マルチスレッドは救いません）
 
 We've used a single-threaded runtime as the execution context in our
 previous example, but the same risk persists even when using a multithreaded
@@ -112,18 +143,31 @@ The only difference is in the number of concurrent tasks required to create the 
 in a single-threaded runtime, 2 are enough; in a multithreaded runtime, we
 would need `N+1` tasks, where `N` is the number of runtime threads.
 
-### Downsides
+> 前の例では実行コンテキストとして単一スレッドランタイムを使用しましたが、同じリスクはマルチスレッドランタイムを使用したときも残ります。
+> 唯一の違いは、デッドロックを作成するために必要な同時並行タスクの数です。
+> 単一スレッドランタイムでは2つで十分でした。
+> マルチスレッドランタイムでは、`N+1`のタスクが必要で、`N`はランタイムスレッドの数です。
+
+### Downsides（欠点）
 
 Having an async-aware `Mutex` comes with a performance penalty.\
 If you're confident that the lock isn't under significant contention
 _and_ you're careful to never hold it across a yield point, you can
 still use `std::sync::Mutex` in an asynchronous context.
 
+> 非同期を理解している`Mutex`を使用することは、パフォーマンスの不利益を伴います。
+> ロックが競合状態でないことに自身があり、`yield`ポイントをまたいでロックを決して保持しないように注意している場合、非同期コンテキストで、まだ`std::sync::Mutex`を使用できます。
+
 But weigh the performance benefit against the liveness risk you
 will incur.
 
-## Other primitives
+> ただし、パフォーマンス上の利点と、発生する非生存リスクを比較検討してください。
+
+## Other primitives（ほかの構成要素）
 
 We used `Mutex` as an example, but the same applies to `RwLock`, semaphores, etc.\
 Prefer async-aware versions when working in an asynchronous context to minimise
 the risk of issues.
+
+> 例として`Mutex`を使用しましたが、`RwLock`、セマフォなどにも同じことが適用されます。
+> 問題のリスクを最小化するために非同期コンテキストでわ行するときは、非同期を理解しているバージョンを優先してください。
--- exercises/08_futures/06_async_aware_primitives/src/lib.rs
+++ exercises/08_futures/06_async_aware_primitives/src/lib.rs
@@ -2,9 +2,13 @@
 ///  which are not async-aware.
 ///  Rewrite it to use `tokio`'s channels primitive (you'll have to touch
 ///  the testing code too, yes).
+/// 非同期を理解していない標準ライブラリのチャネルを使用しているため、下のコードをはデッドロックします。
+/// `tokio`のチャネル構成要素を使用してそれを再記述してください。
+/// テストコードにも触れる必要があります。
 ///
 /// Can you understand the sequence of events that can lead to a deadlock?
-use std::sync::mpsc;
+/// デッドロックを引き起こす可能性があるイベントのシーケンスを理解できますか？
+use tokio::sync::mpsc;
 
 pub struct Message {
     payload: String,
@@ -13,8 +17,111 @@ pub struct Message {
 
 /// Replies with `pong` to any message it receives, setting up a new
 /// channel to continue communicating with the caller.
-pub async fn pong(mut receiver: mpsc::Receiver<Message>) {
+/// 受け取ったメッセージに`pong`で応答して、呼び出し側と会話を継続するために新しいチャネルを準備してください。
+pub async fn pong(mut receiver: mpsc::Receiver<Message>, buffer_size: usize) {
     loop {
+        if let Some(msg) = receiver.recv().await {
+            println!("Pong received: {}", msg.payload);
+            let (sender, new_receiver) = mpsc::channel(buffer_size);
+            msg.response_channel
+                .send(Message {
+                    payload: "pong".into(),
+                    response_channel: sender,
+                })
+                .await
+                .unwrap();
+            receiver = new_receiver;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{pong, Message};
+    use tokio::sync::mpsc;
+
+    #[tokio::test]
+    async fn ping() {
+        let buffer_size = 128;
+        let (sender, receiver) = mpsc::channel(buffer_size);
+        let (response_sender, mut response_receiver) = mpsc::channel(buffer_size);
+
+        let result = sender
+            .send(Message {
+                payload: "pong".into(),
+                response_channel: response_sender,
+            })
+            .await;
+        assert!(result.is_ok());
+
+        tokio::spawn(pong(receiver, buffer_size));
+
+        let answer = response_receiver.recv().await.unwrap().payload;
+        assert_eq!(answer, "pong");
+    }
+}
+
+// # `std::sync::mpsc::Receiver::recv()`
+//
+// この受信者に送信された値を待つことを試みて、対応するチャネルがハングアップした場合エラーを返す。
+// この関数は、利用できるデータがない場合や、少なくとも1つ以上の送信者がまだ存在して、データが送信される可能性がある場合、常にカレントスレッドをブロックする。
+// 一度、メッセージが対応する`Sender`または`SyncSender`に送信されると、この受信者は目覚めてメッセージを送信する。
+// 対応する`Sender`が切断した場合、またはこの呼び出しがブロックしている間に切断した場合、この呼び出しは目覚めさせて、このチャネルで受診できるメッセージがないことを
+// 示す`Err`を返す。
+// しかし、チャネルがバッファーされた場合、切断される前に送信されたメッセージは、適切に受信される。
+
+// 標準ライブラリのmpscがデッドロックする理由は次の通り。
+//
+// `ping`は、`pong`側にメッセージを送信して、`pong`を別スレッドで起動した後で、次の通り`pong`からのレスポンスを待つ。
+//
+// ```rust
+// let answer = response_receiver.recv().unwrap().payload;
+// ```
+//
+// しかし、`recv`は現在のスレッドをブロックして、`pong`に制御が渡らず、そのため`pong`は`ping`にレスポンスを送信できない。
+// よって、`pong`からのレスポンスが得られないため、上記でデッドロックする。
+// この状況は、デバッグプリントで確認できる。
+//
+// ```text
+// before sending in ping
+// after sending in ping
+// before spawning pong
+// after spawning pong
+// before receiving in ping
+// ```
+//
+// 例えば、`pong`を別スレッドで起動した後にスリープすると、`pong`の処理は進むが、`recv()`で停止する。
+// それは、`pong`が`ping`に送信しようとする`msg.response_channel`チャネルが、`ping`と同じスレッドで生成されており、
+// そのスレッドは`recv()`でブロックされているから、送信処理が完了せずにデッドロックしたままである。
+//
+// ```rust
+// tokio::spawn(pong(receiver));
+//
+// tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+//
+// let answer = response_receiver.recv().unwrap().payload;
+// ```
+//
+// ```text
+// before sending in ping
+// after sending in ping
+// before spawning pong
+// after spawning pong
+// before looping in pong
+// before receiving in pong
+// Pong received: pong
+// after sending response in pong
+// before receiving in pong
+// ```
+//
+// これを`tokio`に変更すると、処理が進められなくなった場合、処理を進めようとしたスレッドをブロックする代わりに、
+// 他のタスクに制御を移譲するため、処理が進むようになる。
+// これは、スレッドの同期並行処理（concurrent）と、`tokio`の非同期並列処理（parallel）の違いである。
+/*
+pub async fn pong(mut receiver: mpsc::Receiver<Message>) {
+    println!("before looping in pong");
+    loop {
+        println!("before receiving in pong");
         if let Ok(msg) = receiver.recv() {
             println!("Pong received: {}", msg.payload);
             let (sender, new_receiver) = mpsc::channel();
@@ -24,6 +131,7 @@ pub async fn pong(mut receiver: mpsc::Receiver<Message>) {
                     response_channel: sender,
                 })
                 .unwrap();
+            println!("after sending response in pong");
             receiver = new_receiver;
         }
     }
@@ -38,16 +146,25 @@ mod tests {
     async fn ping() {
         let (sender, receiver) = mpsc::channel();
         let (response_sender, response_receiver) = mpsc::channel();
-        sender
-            .send(Message {
-                payload: "pong".into(),
-                response_channel: response_sender,
-            })
-            .unwrap();
 
+        println!("before sending in ping");
+        let result = sender.send(Message {
+            payload: "pong".into(),
+            response_channel: response_sender,
+        });
+        assert!(result.is_ok());
+        println!("after sending in ping");
+
+        println!("before spawning pong");
         tokio::spawn(pong(receiver));
+        println!("after spawning pong");
 
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+
+        println!("before receiving in ping");
         let answer = response_receiver.recv().unwrap().payload;
+        println!("after receiving in ping");
         assert_eq!(answer, "pong");
     }
 }
+*/
